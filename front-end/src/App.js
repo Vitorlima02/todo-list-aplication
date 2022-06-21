4b163a64e55ea251424c724831e2b45b4293d063
Vitor Lima <vitorliman@gmail.com>
2022-06-21 15:15:10 -0300

    feat: config app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
+import LoginPage from './Pages/LoginPage';
+
 function App() {
   return (
     <main>
       <Routes>
-        <Route exact path="/login"/>
+        <Route exact path="/login" component={ LoginPage } />
       </Routes>
     </main>
   );
